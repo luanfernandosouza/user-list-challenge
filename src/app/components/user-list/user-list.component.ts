bf95654be1dc2e23f3bb9eef4210d80547e24ca5
Luan <luan.souza@iaudit.com.br>
2025-02-07 17:38:04 -0300

    improovements using rxjs

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,37 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { UserlistService } from 'src/services/user-list/user-list.service';
+import { catchError, switchMap, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
 
   users: any[] = [];
   filteredUsers: any[] = [];
   searchTerm: string = '';
+  loading: boolean = false;
+  error: string = '';
 
   constructor(
     private userlistService: UserlistService
   ) { }
 
-  ngOnInit() {
-    this.userlistService.getUserList().subscribe(
-      (data: any) => {
+  ngOnInit(): void {
+    this.fetchUsers();
+  }
+
+  fetchUsers(): void {
+    this.loading = true;
+    this.userlistService.getUserList().pipe(
+      tap((data: any) => {
         this.users = data;
         this.filteredUsers = data;
-      },
-      (error: any) => {
-        console.log(error);
-      }
-    )
+      }),
+      catchError(err => {
+        this.error = 'Failed to load users';
+        this.loading = false;
+        return of([]);
+      })
+    ).subscribe(() => {
+      this.loading = false;
+    });
   }
 
   filterUsers(): void {
-    this.filteredUsers = this.users.filter(user =>
-      user.name.toLowerCase().includes(this.searchTerm.toLowerCase())
-    );
+    if (this.searchTerm.trim()) {
+      this.filteredUsers = this.users.filter(user =>
+        user.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    } else {
+      this.filteredUsers = this.users;
+    }
   }
 
 }
